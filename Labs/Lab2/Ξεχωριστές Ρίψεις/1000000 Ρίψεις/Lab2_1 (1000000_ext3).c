3fb6429b259dcd70990c6b9d574e4b2958c7c135
Joanne Pagounis <72542408+p19pago@users.noreply.github.com>
2022-06-01 21:47:53 +0300

    Add files via upload

---
+++ Labs/Lab2/Ξεχωριστές Ρίψεις/1000000 Ρίψεις/Lab2_1 (1000000_ext3).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char** argv){
+
+	#define N 1000000
+
+	srand(time(NULL));
+	int i, j;
+	int A[6];
+
+	for(i=1; i<=N; i++){
+		for (j=1; j<=6; j++){
+				A[j] = rand()%6 +1;
+				printf("Random integer number: %d\n", A[j]);
+			}
+	}
+		return 0;
+	}
