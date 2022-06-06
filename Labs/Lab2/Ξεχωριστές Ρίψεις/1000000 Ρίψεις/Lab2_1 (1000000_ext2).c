aa58941870953a6b070e708fbd43b0ad7018da87
p19pago <p19pago@ionio.gr>
2022-06-07 00:46:47 +0300

    added to folder

---
+++ Labs/Lab2/Ξεχωριστές Ρίψεις/1000000 Ρίψεις/Lab2_1 (1000000_ext2).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char** argv){
+
+	srand(time(NULL));
+	int i, j;
+	int A[6];
+
+	for(i=1; i<=1000000; i++){
+		for (j=1; j<=6; j++){
+				A[j] = rand()%6 +1;
+				printf("Random integer number: %d\n", A[j]);
+			}
+	}
+		return 0;
+	}
