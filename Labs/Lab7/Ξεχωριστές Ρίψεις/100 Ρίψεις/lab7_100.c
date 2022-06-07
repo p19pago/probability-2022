882912574fe2f53038e5f4b2db99c5eb6ede0c41
Joanne Pagounis <72542408+p19pago@users.noreply.github.com>
2022-06-08 02:04:09 +0300

    Update lab7_100.c

--- Labs/Lab7/Ξεχωριστές Ρίψεις/100 Ρίψεις/lab7_100.c
+++ Labs/Lab7/Ξεχωριστές Ρίψεις/100 Ρίψεις/lab7_100.c
@@ -40,137 +40,138 @@ int sum = 0;
 double sqdiff = 0;
 
 int main(int argc, char** argv){
-	
+
 	//subquery 1
 
 	srand(time(NULL));
+	
+	FILE *fp1;
+	fp1 = fopen("walkers1.txt", "w");
+	
 	for(i=0; i<=100; i++){
 		x = rand()%10+1;
 		printf("Random number result is: %d\n", x);
-		
+
 		if(x>5){
-			
-			pos1 = 500 + 1;
+
+			pos1 = x + 1;
 			s1 = rw[pos1];
-			
+
 			printf("%d\n", s1);
-			
+			fprintf(fp1, "%d\n", pos1);
+
 		}
-		
+
 		if(x<5){
-			
-			pos1 = 500 - 1;
+
+			pos1 = x - 1;
 			s1 = rw[pos1];
-			
+
 			printf("%d\n", s1);
-			
+			fprintf(fp1, "%d\n", pos1);
+
 		}
-		
+
 		printf("%d\n", pos1);
-		
-		FILE *fp1;
-		fp1 = fopen("walkers1.txt", "w");
-		
-		fprintf(fp1, "%d\n", pos1);
 	}
-	
+
 	//subquery 2
-	
+
 	srand(time(NULL));
+	
+	FILE *fp2;
+	fp2 = fopen("walkers2.txt", "w");
+	
 	for(j=0; j<=1000; j++){
 		x = rand()%10+1;
 		printf("Random number result is: %d\n", x);
-		
+
 		if(x>5){
-			
-			pos2 = 500 + 1;
+
+			pos2 = x + 1;
 			s2 = rw[pos2];
-			
+
 			printf("%d\n", s2);
-			
+			fprintf(fp2, "%d\n", pos2);
 		}
-		
+
 		if(x<5){
-			
-			pos2 = 500 - 1;
+
+			pos2 = x - 1;
 			s2 = rw[pos2];
-			
+
 			printf("%d\n", s2);
-			
+			fprintf(fp2, "%d\n", pos2);
 		}
-		
+
 		printf("%d\n", pos2);
-		
-		FILE *fp2;
-		fp2 = fopen("walkers2.txt", "w");
-		
-		fprintf(fp2, "%d\n", pos2);
+
 	}
-	
+
 	//subquery 3
-	
+
 	for(k=1; k<=10; k++){
-		
+
 		r = rand()%10 +1;
 		printf("Random integer number: %d\n", r);
-		
+
 		if(r>5){
-			
-			pos_h = 500 + 1;
+
+			pos_h = r + 1;
 			s3 = rw[pos_h];
-			
+
 			printf("%d\n", s3);
 			h++;
 		}
-		
+
 		printf("%d\n", pos_h);
-		
-		if(x<5){
-			
-			pos_t = 500 - 1;
+
+		if(r<5){
+
+			pos_t = r - 1;
 			s4 = rw[pos_t];
-			
+
 			printf("%d\n", s4);
 			t++;
 		}
-		
+
 		printf("%d\n", pos_t);
-		
+
 		// variance - mean
-		
+
 		// 1. variance
-		
+
 		// for heads
-		
+
 		sum = sum + pos_h;
 		sqdiff = sqdiff + (pos_h - m) * (pos_h - m);
-		
+
 		v = sqdiff/k;
-		
+
 		printf("%f\n", v);
-		
+
 		// for tails
-		
+
 		sum = sum + pos_t;
 		sqdiff = sqdiff + (pos_t - m) * (pos_t - m);
-		
+
 		v = sqdiff/k;
-		
+
 		printf("%f\n", v);
-		
+
 		// 2. mean
-		
+
 		// for heads
-		
+
 		sum = sum + pos_h;
 		m = sum/k;
 		printf("%f\n", m);
-		
+
 		// for tails
 		sum = sum + pos_t;
 		m = sum/k;
 		printf("%f\n", m);
 	}
-	
+
 	return 0;
 }
