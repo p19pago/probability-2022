7ce1cfa86154e81aee8a91dfb9255f33586bf469
Joanne Pagounis <72542408+p19pago@users.noreply.github.com>
2022-06-07 23:54:05 +0300

    Add files via upload

---
+++ Labs/Lab3/Ξεχωριστές Ρίψεις/Πειραγμένο Κέρμα/1000 Ρίψεις/Lab3_3 (1000).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char** argv){
+
+	int pith;
+	int i=0;
+
+	// int h, t;
+
+	for(i=1; i<=1000; i++){
+		pith = rand()%10 + 1;
+		if (pith>8){
+			printf ("A \n"); // h++;
+		} else{
+			printf("B \n"); //t++;
+		}
+	}
+
+	return 0;
+}
