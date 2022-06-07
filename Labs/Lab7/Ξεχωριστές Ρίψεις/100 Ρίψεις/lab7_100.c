f9e8dea3e9cbf82cad6316e70e2c8ed27ee157c0
Joanne Pagounis <72542408+p19pago@users.noreply.github.com>
2022-06-08 02:13:46 +0300

    Update lab7_100.c

--- Labs/Lab7/Ξεχωριστές Ρίψεις/100 Ρίψεις/lab7_100.c
+++ Labs/Lab7/Ξεχωριστές Ρίψεις/100 Ρίψεις/lab7_100.c
@@ -115,32 +115,29 @@ int main(int argc, char** argv){
 		r = rand()%10 +1;
 		printf("Random integer number: %d\n", r);
 
-		if(r>5){
+		if(r>7){
 
 			pos_h = r + 1;
 			s3 = rw[pos_h];
 
 			printf("%d\n", s3);
 			h++;
+			
+			printf("%d\n", pos_h);
 		}
-
-		printf("%d\n", pos_h);
-
-		if(r<5){
+		else{ // r<7
 
 			pos_t = r - 1;
 			s4 = rw[pos_t];
 
 			printf("%d\n", s4);
 			t++;
+			
+			printf("%d\n", pos_t);
 		}
 
-		printf("%d\n", pos_t);
-
 		// variance - mean
-
 		// 1. variance
-
 		// for heads
 
 		sum = sum + pos_h;
@@ -160,7 +157,6 @@ int main(int argc, char** argv){
 		printf("%f\n", v);
 
 		// 2. mean
-
 		// for heads
 
 		sum = sum + pos_h;
